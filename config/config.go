9622a899a9137b74eaf8975f1c68e03deb25dcc6
jj1980a <j.feral@live.fr>
2024-02-28 12:33:38 +0100

    add cli flags

--- config/config.go
+++ config/config.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"encoding/json"
+	"flag"
 	"math/big"
 	"os"
 
@@ -14,9 +15,9 @@ type Config struct {
 	} `json:"network"`
 
 	Contracts *struct {
-		Atlas             common.Address `json:"atlas"`
-		AtlasVerification common.Address `json:"atlasVerification"`
-		Simulator         common.Address `json:"simulator"`
+		Atlas             common.Address `json:"atlas,omitempty"`
+		AtlasVerification common.Address `json:"atlasVerification,omitempty"`
+		Simulator         common.Address `json:"simulator,omitempty"`
 	} `json:"contracts"`
 
 	Relay *struct {
@@ -42,9 +43,53 @@ func Load() *Config {
 		panic(err)
 	}
 
+	config.parseFlags()
+
 	if envRpcUrl, set := os.LookupEnv("ETH_RPC_URL"); set && len(envRpcUrl) > 0 {
 		config.Network.RpcUrl = envRpcUrl
 	}
 
 	return &config
+}
+
+func (c *Config) parseFlags() {
+	networkRpcUrlPtr := flag.String("network.rpc_url", "", "Ethereum RPC URL")
+	contractsAtlasPtr := flag.String("contracts.atlas", "", "Atlas contract address")
+	contractsAtlasVerificationPtr := flag.String("contracts.atlasVerification", "", "AtlasVerification contract address")
+	contractsSimulatorPtr := flag.String("contracts.simulator", "", "Simulator contract address")
+	relayGasMaxPerUserOperationPtr := flag.String("relay.gas.max_per_user_operation", "", "Max gas per user operation")
+	relayGasMaxPerSolverOperationPtr := flag.String("relay.gas.max_per_solver_operation", "", "Max gas per solver operation")
+	relayGasMaxPerDAppOperationPtr := flag.String("relay.gas.max_per_dApp_operation", "", "Max gas per dApp operation")
+	flag.Parse()
+
+	if len(*networkRpcUrlPtr) > 0 {
+		c.Network.RpcUrl = *networkRpcUrlPtr
+	}
+
+	if len(*contractsAtlasPtr) > 0 {
+		c.Contracts.Atlas = common.HexToAddress(*contractsAtlasPtr)
+	}
+
+	if len(*contractsAtlasVerificationPtr) > 0 {
+		c.Contracts.AtlasVerification = common.HexToAddress(*contractsAtlasVerificationPtr)
+	}
+
+	if len(*contractsSimulatorPtr) > 0 {
+		c.Contracts.Simulator = common.HexToAddress(*contractsSimulatorPtr)
+	}
+
+	if len(*relayGasMaxPerUserOperationPtr) > 0 {
+		c.Relay.Gas.MaxPerUserOperation = new(big.Int)
+		c.Relay.Gas.MaxPerUserOperation.SetString(*relayGasMaxPerUserOperationPtr, 10)
+	}
+
+	if len(*relayGasMaxPerSolverOperationPtr) > 0 {
+		c.Relay.Gas.MaxPerSolverOperation = new(big.Int)
+		c.Relay.Gas.MaxPerSolverOperation.SetString(*relayGasMaxPerSolverOperationPtr, 10)
+	}
+
+	if len(*relayGasMaxPerDAppOperationPtr) > 0 {
+		c.Relay.Gas.MaxPerDAppOperation = new(big.Int)
+		c.Relay.Gas.MaxPerDAppOperation.SetString(*relayGasMaxPerDAppOperationPtr, 10)
+	}
 }
