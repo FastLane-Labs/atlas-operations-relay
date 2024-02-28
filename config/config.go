eb6ebe71720b372d46ad09a73150188424c9fb07
jj1980a <j.feral@live.fr>
2024-02-28 22:14:22 +0100

    config params validation

--- config/config.go
+++ config/config.go
@@ -51,12 +51,11 @@ type Config struct {
 
 func Load() *Config {
 	config := &Config{}
+
 	config.parseConfigFile()
 	config.parseFlags()
-
-	if envRpcUrl, set := os.LookupEnv("ETH_RPC_URL"); set && len(envRpcUrl) > 0 {
-		config.Network.RpcUrl = envRpcUrl
-	}
+	config.parseEnv()
+	config.validate()
 
 	return config
 }
@@ -80,14 +79,23 @@ func (c *Config) parseConfigFile() {
 	}
 
 	if len(configJson.Contracts.Atlas) > 0 {
+		if !common.IsHexAddress(configJson.Contracts.Atlas) {
+			panic("contracts.atlas is not a valid address")
+		}
 		c.Contracts.Atlas = common.HexToAddress(configJson.Contracts.Atlas)
 	}
 
 	if len(configJson.Contracts.AtlasVerification) > 0 {
+		if !common.IsHexAddress(configJson.Contracts.AtlasVerification) {
+			panic("contracts.atlasVerification is not a valid address")
+		}
 		c.Contracts.AtlasVerification = common.HexToAddress(configJson.Contracts.AtlasVerification)
 	}
 
 	if len(configJson.Contracts.Simulator) > 0 {
+		if !common.IsHexAddress(configJson.Contracts.Simulator) {
+			panic("contracts.simulator is not a valid address")
+		}
 		c.Contracts.Simulator = common.HexToAddress(configJson.Contracts.Simulator)
 	}
 
@@ -111,14 +119,23 @@ func (c *Config) parseFlags() {
 	}
 
 	if len(*contractsAtlasPtr) > 0 {
+		if !common.IsHexAddress(*contractsAtlasPtr) {
+			panic("contracts.atlas is not a valid address")
+		}
 		c.Contracts.Atlas = common.HexToAddress(*contractsAtlasPtr)
 	}
 
 	if len(*contractsAtlasVerificationPtr) > 0 {
+		if !common.IsHexAddress(*contractsAtlasVerificationPtr) {
+			panic("contracts.atlasVerification is not a valid address")
+		}
 		c.Contracts.AtlasVerification = common.HexToAddress(*contractsAtlasVerificationPtr)
 	}
 
 	if len(*contractsSimulatorPtr) > 0 {
+		if !common.IsHexAddress(*contractsSimulatorPtr) {
+			panic("contracts.simulator is not a valid address")
+		}
 		c.Contracts.Simulator = common.HexToAddress(*contractsSimulatorPtr)
 	}
 
@@ -135,5 +152,29 @@ func (c *Config) parseFlags() {
 	if len(*relayGasMaxPerDAppOperationPtr) > 0 {
 		c.Relay.Gas.MaxPerDAppOperation = new(big.Int)
 		c.Relay.Gas.MaxPerDAppOperation.SetString(*relayGasMaxPerDAppOperationPtr, 10)
+	}
+}
+
+func (c *Config) parseEnv() {
+	if envRpcUrl, set := os.LookupEnv("ETH_RPC_URL"); set && len(envRpcUrl) > 0 {
+		c.Network.RpcUrl = envRpcUrl
+	}
+}
+
+func (c *Config) validate() {
+	if len(c.Network.RpcUrl) == 0 {
+		panic("network.rpc_url is required")
+	}
+
+	if c.Contracts.Atlas == (common.Address{}) {
+		panic("contracts.atlas is required")
+	}
+
+	if c.Contracts.AtlasVerification == (common.Address{}) {
+		panic("contracts.atlasVerification is required")
+	}
+
+	if c.Contracts.Simulator == (common.Address{}) {
+		panic("contracts.simulator is required")
 	}
 }
