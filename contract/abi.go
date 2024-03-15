c2d48d56769e9721cdb96b25f91b12ecabc9d789
jj1980a <j.feral@live.fr>
2024-03-15 10:14:26 +0100

    simulate metacall for bundles instead of using the simulator

--- contract/abi.go
+++ contract/abi.go
@@ -1,12 +1,14 @@
 package contract
 
 import (
+	"github.com/FastLane-Labs/atlas-operations-relay/contract/atlas"
 	"github.com/FastLane-Labs/atlas-operations-relay/contract/dAppControl"
 	"github.com/FastLane-Labs/atlas-operations-relay/contract/simpleRfqSolver"
 	"github.com/FastLane-Labs/atlas-operations-relay/contract/simulator"
 )
 
 var (
+	AtlasAbi, _           = atlas.AtlasMetaData.GetAbi()
 	DappControlAbi, _     = dAppControl.DAppControlMetaData.GetAbi()
 	SimulatorAbi, _       = simulator.SimulatorMetaData.GetAbi()
 	SimpleRfqSolverAbi, _ = simpleRfqSolver.SimpleRfqSolverMetaData.GetAbi()
