345d611f1f7ae2672b3391a0703e51f104caaa55
Amitabh Anand <amitabh009@gmail.com>
2024-03-17 15:07:51 +0530

    change sepolia addresses for integration tests

--- tests/configs.go
+++ tests/configs.go
@@ -36,9 +36,9 @@ var (
 			RpcUrl: "https://rpc.sepolia.org/",
 		},
 		Contracts: config.Contracts{
-			Atlas:             common.HexToAddress("0x090A1134ebF3066f87dB165a910E832a87C2Ffb5"),
-			AtlasVerification: common.HexToAddress("0x15261B0437b0e54d6f0Dd308148a359Bb0191A81"),
-			Simulator:         common.HexToAddress("0x8F4C1158fA69f973973b6ba7A1b3F58EE9204d1A"),
+			Atlas:             common.HexToAddress("0x79d1379195f1Ed373eF8c58aC36F9C1045f8684d"),
+			AtlasVerification: common.HexToAddress("0xf8e760554aB5c7E1Da51fFeD9C48de78bddD4c53"),
+			Simulator:         common.HexToAddress("0x165877D0E2646bf7B42621D1551a23b94B14EfF9"),
 		},
 		Relay: config.Relay{
 			Auction: config.Auction{
